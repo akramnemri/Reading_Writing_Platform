641ed476babe748d4b2c6d7c12b6cde5f5a83e83
akramnemri <florensiahiro1@gmail.com>
2026-03-18 04:59:25 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reading_Writing_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
